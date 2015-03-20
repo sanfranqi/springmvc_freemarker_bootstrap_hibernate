99f6bf5127b5d402a7e9e5755a02b2cc343d5934
sanfranqi <85910869@qq.com>
2015-03-20 18:03:09 +0800

    edit

--- Spring4.0.2Hibernate4.3Freemarker2.3.20/src/com/bufoon/controller/UserController.java
+++ Spring4.0.2Hibernate4.3Freemarker2.3.20/src/com/bufoon/controller/UserController.java
@@ -29,24 +29,25 @@ public class UserController {
 
 	@Resource
 	private UserService userService;
-	
+
 	@RequestMapping("/userLogin")
-	public String userLogin(UserModel userModel, Model model){
+	public String userLogin(UserModel userModel, Model model) {
 		userModel.setPassword(Util.encryptMD5(userModel.getPassword()));
 		User user = userService.findUserByByCondition(userModel);
 		if (user == null) {
 			model.addAttribute("errorInfo", "you password is error!");
 			return "login";
 		}
-		
+
 		Constants.LOGIN_USER = user.getUsername();
 		return "redirect:/user/index";
 	}
+
 	@RequestMapping("/addUser")
-	public String addUser(UserModel userModel){
+	public String addUser(UserModel userModel) {
 		Date date = new Date();
 		User user = new User();
-		BeanUtils.copyProperties(userModel, user); //实体属性复制，将userModel中的属性值复制到User中
+		BeanUtils.copyProperties(userModel, user); // 实体属性复制，将userModel中的属性值复制到User中
 		user.setPassword(Util.encryptMD5(userModel.getPassword()));
 		user.setCreateTime(date);
 		user.setUpdateTime(date);
@@ -55,32 +56,33 @@ public class UserController {
 		userService.saveUser(user);
 		return "redirect:/user/index";
 	}
+
 	@RequestMapping("/index")
-	public ModelAndView index(UserModel userModel, HttpServletRequest request) {  
-		if(Util.isNull(Constants.LOGIN_USER)){
+	public ModelAndView index(UserModel userModel, HttpServletRequest request) {
+		if (Util.isNull(Constants.LOGIN_USER)) {
 			ModelAndView mav = new ModelAndView("login");
 			mav.addObject("errorInfo", "user is overdue!");
 			return mav;
 		}
-	    return new ModelAndView("userList");  
-	 }  
-	
+		return new ModelAndView("userList");
+	}
+
 	@RequestMapping("/getUser")
-	public void getUser(HttpServletRequest request, HttpServletResponse response, Model model) {  
-	    int id = Integer.parseInt(request.getParameter("userId"));
-	    User user = userService.getUserById(id);
-	    Map<String, Object> map = new HashMap<String, Object>();
-	    map.put("user", user);
-	    model.addAttribute("user", user);
-	 }  
-	
+	public void getUser(HttpServletRequest request, HttpServletResponse response, Model model) {
+		int id = Integer.parseInt(request.getParameter("userId"));
+		User user = userService.getUserById(id);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("user", user);
+		model.addAttribute("user", user);
+	}
+
 	@RequestMapping("/updateUser")
-	public String updateUser(UserModel userModel){
+	public String updateUser(UserModel userModel) {
 		Date date = new Date();
 		User user = userService.getUserById(userModel.getId());
 		if (Util.isNull(userModel.getPassword())) {
 			userModel.setPassword(user.getPassword());
-		}else {
+		} else {
 			userModel.setPassword(Util.encryptMD5(userModel.getPassword()));
 		}
 		userModel.setCreateTime(user.getCreateTime());
@@ -91,21 +93,22 @@ public class UserController {
 		userService.updateUser(user);
 		return "redirect:/user/index";
 	}
-	
+
 	@RequestMapping("/deleteUser")
-	public String deleteUser(HttpServletRequest request){
-		  int id = Integer.parseInt(request.getParameter("userId"));
-		  User user = userService.getUserById(id);
-		  userService.deleteUser(user);
-		  return "redirect:/user/index";
+	public String deleteUser(HttpServletRequest request) {
+		int id = Integer.parseInt(request.getParameter("userId"));
+		User user = userService.getUserById(id);
+		userService.deleteUser(user);
+		return "redirect:/user/index";
 	}
-	
+
 	@RequestMapping("/exit")
-	public String exit(){
+	public String exit() {
 		Constants.LOGIN_USER = null;
 		return "redirect:/";
 	}
-	//返回json
+
+	// 返回json
 	@RequestMapping("/getJsonUserList")
 	@ResponseBody
 	public ModelMap getJsonUserList(UserModel userModel, HttpServletRequest request, HttpServletResponse response) {
@@ -117,11 +120,21 @@ public class UserController {
 			page = Integer.parseInt(sPage);
 		}
 		PageResultSet<User> userPageResult = userService.findPageUserList(userModel, page, Constants.PAGE_SIZE);
-		
-		modelMap.addAttribute("event",userPageResult.getList());
+
+		modelMap.addAttribute("event", userPageResult.getList());
 		modelMap.addAttribute("pageBean", userPageResult.getPage());
-		modelMap.addAttribute("pageCount",userPageResult.getPage().getTotalPage());
+		modelMap.addAttribute("pageCount", userPageResult.getPage().getTotalPage());
 		return modelMap;
 	}
-	  
+
+	@RequestMapping("/test")
+	public ModelAndView test() {
+		ModelAndView modelAndView = new ModelAndView();
+		UserModel userModel = new UserModel();
+		PageResultSet<User> userPageResult = userService.findPageUserList(userModel, 1, Constants.PAGE_SIZE);
+		modelAndView.addObject("users", userPageResult.getList());
+		modelAndView.setViewName("test");
+		return modelAndView;
+	}
+
 }
